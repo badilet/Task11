6a8694ae43f12ea0cfc7c8b469d6893ecac3bd8b
User <user@Users-MacBook-Pro-235.local>
2019-12-20 23:57:25 -0600

    first commit

---
+++ Task11.py
@@ -0,0 +1,6 @@
+# Given a string consisting of words separated by spaces. Determine how many
+# words it has. To solve the problem, use the method count.
+
+quote = "Everybody in this country should learn programming, because it teaches how to think!"
+print(quote)
+print("Number of words:", len(quote.split()))
